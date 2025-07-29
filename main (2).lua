1d0f66daa1522a3efb63d4a854d88265a5e90462
dream77239 <2775720154@qq.com>
2025-07-29 13:54:21 +0800

    Add files via upload

--- main (2).lua
+++ main (2).lua
@@ -6577,7 +6577,7 @@ AutomaticSize="XY",
 BackgroundTransparency=1,
 },{
 b("TextLabel",{
-Text=o.User.Anonymous and"Anonymous"or game.Players.LocalPlayer.DisplayName,
+Text=o.User.aaa and"aaa"or game.Players.LocalPlayer.DisplayName,
 TextSize=17,
 ThemeTag={
 TextColor3="Text",
